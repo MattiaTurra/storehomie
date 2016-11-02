3c5ab53ee4e91be0c475c26551e60dd080d2820e
naturralien <mattia.turra@gmx.ch>
2016-11-02 22:46:30 +0100

    Minor Patch

--- app/src/main/java/ch/zli/storehomie/app/FilterScreen.java
+++ app/src/main/java/ch/zli/storehomie/app/FilterScreen.java
@@ -1,7 +1,11 @@
 package ch.zli.storehomie.app;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FilterScreen extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class FilterScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_screen);
+    }
+
+    public void onSuggestionClick (View view)
+    {
+        TextView i = (TextView)findViewById(view.getId());
+        EditText t = (EditText)findViewById(R.id.SearchTextbox);
+        t.setText(i.getText());
     }
 }
